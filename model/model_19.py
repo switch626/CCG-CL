49681dda218a55564f6d0502651a07ef6c51de66
Xianglong Wang <37759366+switch626@users.noreply.github.com>
2024-04-24 17:57:05 +0800

    Update model_19.py

--- model/model_19.py
+++ model/model_19.py
@@ -1,7 +1,7 @@
 # 1. Load packages
 import os
 import torch
-import pyramid
+import model.pyramid
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
@@ -11,7 +11,7 @@ import torchvision.models.resnet as res
 from math import pi
 from functools import partial
 from yacs.config import CfgNode as CN
-from global_model import global_module
+from model.global_model import global_module
 from torch.hub import load_state_dict_from_url
 
 # 2. Define the functions
